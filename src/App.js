0709e6d46d99c8db520909501699eb1b4ab6db5a
Kurylenko Serhii <sergey25111988@gmail.com>
2021-08-02 12:44:15 +0300

    hm2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
 import './App.css';
+import Feedback from './components/Feedback/Feedback';
 
 
 const App =()=> {
   return (
    
-    <div>
-     ddsfdsfdsf
-    </div>
+    <Feedback/>
   );
 }
 
